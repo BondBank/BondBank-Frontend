71dc34b7e580b4c349bafcdffe9b58c323e26945
levblanc <levblanc@gmail.com>
2022-11-13 15:14:09 +0800

    Add Material UI dependencies & font CSS

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Link from 'next/link'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '../styles/globals.css';
+
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
 //import {Web3ReactProvider} from ''
 
 function MyApp({ Component, pageProps }: AppProps) {
